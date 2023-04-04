f54cee111789d4be6a05696a0327a745460f6763
zsero-penguin <arato1060@outlook.jp>
2023-04-04 15:38:16 +0900

    page not found　修正

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+#post_detailのビューの追加
+def post_detail(request, pk):
+    #page not foundに対して
+    post = get_object_or_404(Post, pk=pk)
+    #post_detailのURLをプロパティとしてHTMLファイルとして返す。
+    return render(request, 'blog/post_detail.html', {'post': post})
