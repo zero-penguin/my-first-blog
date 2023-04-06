35350e373983419190a3da563bce178f739259b3
zsero-penguin <arato1060@outlook.jp>
2023-04-06 22:49:35 +0900

    セキュアの追加

--- blog/urls.py
+++ blog/urls.py
@@ -1,14 +1,14 @@
 """
 path関数とblogアプリのすべてのインポート
 """
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
 
-#http付きのURLへの対応
+from django.contrib.auth import views
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('', include('blog.urls')),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,15 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+# ユーザーログイン機能をインポート
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+@login_required
 #post_detailのビューの追加
 def post_detail(request, pk):
     #page not foundに対して
@@ -16,6 +20,7 @@ def post_detail(request, pk):
     #post_detail.htmlのテンプレートを返す。。
     return render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 # PostForm()を呼び出しテンプレートに渡す
 def post_new(request):
     if request.method == "POST":
@@ -30,6 +35,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 # メインページのviewに編集を追加
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
